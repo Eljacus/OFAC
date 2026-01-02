cd6ba6d2e70c483a46973d28d4e91a7be3933376
Jacob Arevalo <arevalojonathan230@gmail.com>
2026-01-01 22:33:14 -0500

    Add diagram and video

--- test/testConnection.py
+++
@@ -1,6 +0,0 @@
-from sqlalchemy import text
-from db.connection import engine
-
-with engine.connect() as conn:
-    result = conn.execute(text("SELECT 1"))
-    print(result.scalar())
--- test/testNavOfac.py
+++
@@ -1,8 +0,0 @@
-from utils.ManagementScreen import ManagementScreen
-
-ofac = ManagementScreen()
-ofac.openBrowser()
-
-amount= ofac.searchPerson("PEDRO AREVALO")
-print("Resultados encontrados:", amount)
-
